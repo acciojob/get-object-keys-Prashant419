d86800742caf0e6abc85b2002b26c40e5869dda3
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2024-02-09 17:06:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Creating the student object
+const student = {
+  name: 'John'
+};
+
+// Adding getKeys method to Object prototype
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Example usage
+const keys = student.getKeys();
+console.log(keys); // Output: ['name']
